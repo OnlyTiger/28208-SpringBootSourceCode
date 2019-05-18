655fd58fb031e528907572aeea9f0bde1feedb66
OnlyTiger <15927561814@163.com>
2019-05-18 21:43:08 +0800

    新增测试！ 2019年5月18日21:43:03

---
+++ SourceCode/highlight_springmvc4/src/main/java/com/wisely/highlight_springmvc4/TsteDemo.java
@@ -0,0 +1,24 @@
+/**
+ * Copyright © 2019 湖北航天信息技术有限公司. 保留所有权利。
+ *
+ * @ClassName: TsteDemo.java
+ * @Package com.wisely.highlight_springmvc4
+ * @Description: TODO 描述
+ * @author zhanghu
+ * @date 2019年5月18日 下午9:42:18
+ * @version
+ */
+package com.wisely.highlight_springmvc4;
+
+/**
+ * @ClassName: TsteDemo.java
+ * @Description: TODO 描述
+ * @author zhanghu
+ * @date 2019年5月18日 下午9:42:18
+ */
+public class TsteDemo {
+    public static void main(String[] args) {
+        System.out.println("测试代码！");
+    }
+
+}
