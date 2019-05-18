e31e6f1e909692aba33496fe92ce464466c4a4c1
OnlyTiger <15927561814@163.com>
2019-05-19 01:29:53 +0800

    1.3.2Java配置。 @Configuration声明当前类是一个而配置类，相当于一个Spring配置的xml文件。 使用注解可以代替xml配置。

--- SourceCode/highlight_spring4/src/main/java/com/wisely/highlight_spring4/ch1/javaconfig/JavaConfig.java
+++ SourceCode/highlight_spring4/src/main/java/com/wisely/highlight_spring4/ch1/javaconfig/JavaConfig.java
@@ -3,25 +3,25 @@ package com.wisely.highlight_spring4.ch1.javaconfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration //1
+@Configuration // 1、使用@Configuration注解表明当前类是一个而配置类，这意味着这个类中可能有0个或者多个@Bean注解，此处没有使用包扫描，是因为所有的Bean都在此类中定义了。
 public class JavaConfig {
-	@Bean //2
-	public FunctionService functionService(){
-		return new FunctionService();
-	}
-	
-	@Bean 
-	public UseFunctionService useFunctionService(){
-		UseFunctionService useFunctionService = new UseFunctionService();
-		useFunctionService.setFunctionService(functionService()); //3
-		return useFunctionService;
-		
-	}
+    @Bean // 2、使用@Bean注解声明当方法FunctionService的返回值是一个Bean，Bean的名称是方法名。
+    public FunctionService functionService() {
+        return new FunctionService();
+    }
 
-//	@Bean 
-//	public UseFunctionService useFunctionService(FunctionService functionService){//4
-//		UseFunctionService useFunctionService = new UseFunctionService();
-//		useFunctionService.setFunctionService(functionService);
-//		return useFunctionService;
-//	}
+    @Bean
+    public UseFunctionService useFunctionService() {
+        UseFunctionService useFunctionService = new UseFunctionService();
+        useFunctionService.setFunctionService(functionService()); // 3、注入FunctionService的Bean的时候直接调用functionService()。
+        return useFunctionService;
+
+    }
+
+//    @Bean
+//    public UseFunctionService useFunctionService(FunctionService functionService) {// 4、另外一种注入方式，直接将FunctionService作为参数给useFunctionService()。
+//        UseFunctionService useFunctionService = new UseFunctionService();
+//        useFunctionService.setFunctionService(functionService);
+//        return useFunctionService;
+//    }
 }
