ef462c010e979fdfa66e4b42ab2bb1963f90102e
OnlyTiger <15927561814@163.com>
2019-05-19 11:56:36 +0800

    2.4Profile用户配置文件 2019年5月19日11:56:25

--- SourceCode/highlight_spring4/src/main/java/com/wisely/highlight_spring4/ch2/profile/Main.java
+++ SourceCode/highlight_spring4/src/main/java/com/wisely/highlight_spring4/ch2/profile/Main.java
@@ -4,18 +4,17 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class Main {
 
-	public static void main(String[] args) {
-		  AnnotationConfigApplicationContext context =  
-				  new AnnotationConfigApplicationContext();
-		  
-		  context.getEnvironment().setActiveProfiles("dev"); //1
-		  context.register(ProfileConfig.class);//2
-		  context.refresh(); //3
-		  
-	      DemoBean demoBean = context.getBean(DemoBean.class);
-	      
-	      System.out.println(demoBean.getContent());
-	      
-	      context.close();
-	}
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+
+        context.getEnvironment().setActiveProfiles("dev"); // 1、将活动的Profiles设置为dev
+        context.register(ProfileConfig.class);// 2、后置注册Bean配置类
+        context.refresh(); // 3、刷新容器。
+
+        DemoBean demoBean = context.getBean(DemoBean.class);
+
+        System.out.println(demoBean.getContent());
+
+        context.close();
+    }
 }
--- SourceCode/highlight_spring4/src/main/java/com/wisely/highlight_spring4/ch2/profile/ProfileConfig.java
+++ SourceCode/highlight_spring4/src/main/java/com/wisely/highlight_spring4/ch2/profile/ProfileConfig.java
@@ -4,18 +4,24 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+/**
+ * @ClassName: ProfileConfig.java
+ * @Description: 用户配置文件。Profile配置。Profile为在不同环境下使用不同的配置提供了支持。（比如开发环境，生产环境）
+ * @author zhanghu
+ * @date 2019年5月19日 上午11:50:29
+ */
 @Configuration
 public class ProfileConfig {
-	@Bean
-	@Profile("dev") //1
-	public DemoBean devDemoBean() {
-		return new DemoBean("from development profile");
-	}
+    @Bean
+    @Profile("dev") // 1、Profile为dev时，实例化devDemoBean
+    public DemoBean devDemoBean() {
+        return new DemoBean("from development profile");
+    }
 
-	@Bean
-	@Profile("prod") //2
-	public DemoBean prodDemoBean() {
-		return new DemoBean("from production profile");
-	}
+    @Bean
+    @Profile("prod") // 2、Profile为prod时，实例化prodDemoBean
+    public DemoBean prodDemoBean() {
+        return new DemoBean("from production profile");
+    }
 
 }
